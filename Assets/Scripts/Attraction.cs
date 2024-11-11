15c4944215ae63016372bec90aec20aa6765d09e
Ilya Molchanov <lolkers.ilya@gmail.com>
2024-11-11 16:40:10 +0300

    make logic for gravity

--- Assets/Scripts/Attraction.cs
+++ Assets/Scripts/Attraction.cs
@@ -1,65 +1,31 @@
-// using System.Collections.Generic;
-// using System.Linq;
-// using UnityEditor;
-// using UnityEngine;
+using System;
+using UnityEngine;
 
-// public class Attraction : MonoBehaviour
-// {
-//     public bool NextPlanet { get; set; }
-//     [SerializeField] private Transform pivot;
-//     [SerializeField] private Rigidbody2D rb;
-//     [SerializeField] private float strengthMultiplier = 3;
-//     private List<Rigidbody2D> _affectedBodies = new List<Rigidbody2D>();
-
-//     private void Update()
-//     {
-//         Debug.Log($"Update {_affectedBodies.Count}");
-
-//         if (_affectedBodies.Count > 0)
-//         {
-//             foreach (Rigidbody2D body in _affectedBodies.ToList())
-//             {
-//                 if (body.CompareTag(CompareTags.Player))
-//                 {
-//                     pivot.position = body.position;
-//                     Vector2 position = transform.position;
-//                     Vector2 direction = (position - body.position).normalized;
-//                     float distance = (position - body.position).magnitude;
-//                     float strength = strengthMultiplier * body.mass * rb.mass / distance;
-
-//                     body.AddForce(direction * strength);
-
-//                     if (distance < 1)
-//                     {
-//                         body.transform.SetParent(transform);
-//                         body.position = pivot.position;
-//                         body.GetComponent<Constraint>().enabled = true;
-//                         body.linearVelocity = Vector2.zero;
-//                         _affectedBodies.Remove(body);
-//                         MessageAboutNextPlanet.Instance.NextPlanetEvent();
-//                     }
-//                 }
-//             }
-//         }
-
-//     }
-//     private void OnTriggerEnter2D(Collider2D other)
-//     {
-//         if (other.TryGetComponent(out Rigidbody2D rb) && other.CompareTag(CompareTags.Player))
-//         {
-//             Debug.Log("Trigger enter + " + rb.name);
-//             _affectedBodies.Add(rb);
-//         }
-//     }
-
-//     private void OnTriggerExit2D(Collider2D other)
-//     {
-//         if (other.TryGetComponent(out Rigidbody2D rb))
-//         {
-//             if (_affectedBodies != null && _affectedBodies.Contains(rb))
-//             {
-//                 _affectedBodies.Remove(rb);
-//             }
-//         }
-//     }
-// }
+public class Attraction : MonoBehaviour
+{
+    private Collider2D _collider;
+    private void Awake()
+    {
+        _collider = GetComponent<Collider2D>();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(CompareTags.Player))
+        {
+            Debug.Log("Trigger enter + " + other.name + " in " + name + " TESTING");
+            GameObject player = other.gameObject;
+            if (player.TryGetComponent(out Rigidbody2D rb))
+            {
+                rb.bodyType = RigidbodyType2D.Dynamic;
+                rb.linearVelocity = Vector2.zero;
+                Debug.Log("NextPlanetEventZXC");
+                NextPlanetEvent();
+            }
+            _collider.enabled = false;
+        }
+    }
+    private void NextPlanetEvent()
+    {
+        MessageAboutNextPlanet.Instance.NextPlanetEvent();
+    }
+}
---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    [SerializeField] private float gravityRadius = 5f;
+    [SerializeField] private float gravityForce = 9.81f;
+    private Rigidbody2D _rb;
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        ApplyGravity();
+    }
+
+    private void ApplyGravity()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, gravityRadius);
+        foreach (Collider2D collider in colliders)
+        {
+            Rigidbody2D rb = collider.GetComponent<Rigidbody2D>();
+            if (rb != null && rb.gameObject != this.gameObject)
+            {
+                Vector2 position = transform.position;
+                Vector2 direction = (transform.position - collider.transform.position).normalized;
+                float distance = (position - rb.position).magnitude;
+                float strength = gravityForce * rb.mass * _rb.mass / distance;
+                rb.AddForce(direction * strength);
+                if (distance < 1)
+                {
+                    rb.bodyType = RigidbodyType2D.Kinematic;
+                }
+            }
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, gravityRadius);
+    }
+}
